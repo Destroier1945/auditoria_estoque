64a2f045b40bce8b8f97bbbc56d451279beaa5b5
Juliano <kluge233@hotmail.com>
2024-06-30 19:34:58 -0300

    Add sqflite and provider

---
+++ lib/model/produto.dart
@@ -0,0 +1,13 @@
+class Produtos {
+  String ean;
+  String description;
+  String complement;
+  String brand;
+
+  Produtos({
+    required this.ean,
+    required this.complement,
+    required this.description,
+    required this.brand
+  });
+}
